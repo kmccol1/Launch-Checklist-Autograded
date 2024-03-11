87677765efeec8888583b05087695434146ffd0a
Kyle McColgan <kdmbus21@gmail.com>
2024-03-11 02:22:05 -0500

    Upload assignment five task two.

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+    // event.preventDefault();
+    const submitButton = document.querySelector('#formSubmit');
+    let myForm = document.querySelector("form");
+    submitButton.addEventListener("click", function(event)
+    {
+        let myList = [0,0];
+        // let pilotName = document.querySelector("input[name=pilotName]");
+        // let copilotName = document.querySelector("input[name=copilotName]");
+        // let fuel = document.querySelector("input[name=fuelLevel]");
+        // let cargo = document.querySelector("input[name=cargoMass]");
+
+        const pilotInputField = document.getElementById("pilotName");
+        const pilotName = pilotInputField.value;
+
+        const copilotInputField = document.querySelector('input[name="copilotName"]');
+        const copilotName = copilotInputField.value;
+
+        const fuelInputField = document.querySelector('input[name="fuelLevel"]');
+        const fuel = fuelInputField.value;
+
+        const cargoInputField = document.querySelector('input[name="cargoMass"]');
+        const cargo = cargoInputField.value;
+        formSubmission(document, myList, pilotName, copilotName, fuel, cargo);
+        form.submit();
+        // event.preventDefault();
+    });
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,6 +1,7 @@
+//import 'cross-fetch/polyfill';
 // Write your helper functions here!
-
 require('cross-fetch/polyfill');
+
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
@@ -20,7 +21,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 function validateInput(testInput)
 {
     let result;
-    if(testInput.length < 1)
+    if(testInput === "")
     {
         result = 'Empty';
     }
@@ -35,10 +36,48 @@ function validateInput(testInput)
     return result;
 }
  
- function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    
+ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel)
+ {
+     //validateInput(pilot)...etc....
+     let result = true;
+     if (validateInput(pilot) == 'Empty' || validateInput(copilot) == 'Empty' || validateInput(fuelLevel) == 'Not a Number' || validateInput(cargoLevel) == 'Not a Number')
+     {
+         alert("All fields are required!");
+         event.preventDefault();
+     }
+
+    let myDiv = document.getElementById('faultyItems');
+    // let listDiv = myDiv.querySelectorAll('li');
+    // listDiv.forEach(function(item){if(item.id=='pilotStatus'){item.textContent=`${pilot}`;}});
+    const pilotStatusElement = document.querySelector('ol #pilotStatus');
+    pilotStatusElement.textContent = `${pilot} Ready`;
+
+    const copilotStatusElement = document.querySelector('ol #copilotStatus');
+    copilotStatusElement.textContent = `${copilot} Ready`;
+
+
+    if ( (fuelLevel < 10000) || (cargoLevel > 10000))
+    {
+        myDiv.style.visibility = 'visible';
+        document.querySelector("h2").textContent = "Shuttle not ready for launch";
+        document.querySelector("h2").style.color = "red";
+        const fuelStatusElement = document.querySelector('ol #fuelStatus');
+        fuelLevel < 10000 ? fuelStatusElement.textContent = "Fuel level not high enough for launch" :fuelStatusElement.textContent = "Fuel level is high enough for launch";
+        const cargoStatusElement = document.querySelector('ol #cargoStatus');
+        cargoLevel > 10000 ? cargoStatusElement.textContent = "Cargo mass too high for launch" :cargoStatusElement.textContent = "Cargo mass is low enough for launch";
+        event.preventDefault();
+    }
+    else
+    {
+        document.querySelector("h2").textContent = "Shuttle is ready for launch";
+        document.querySelector("h2").style.color = "green";
+    }
+
+    event.preventDefault();
+    return result;
  }
- 
+ //task 3 functions commented out below
+ /*
  async function myFetch() {
      let planetsReturned;
  
@@ -50,7 +89,7 @@ function validateInput(testInput)
  
  function pickPlanet(planets) {
  }
- 
+ */
  module.exports.addDestinationInfo = addDestinationInfo;
  module.exports.validateInput = validateInput;
  module.exports.formSubmission = formSubmission;
